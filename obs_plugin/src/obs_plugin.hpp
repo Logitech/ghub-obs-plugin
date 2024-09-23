b01e03cd4933c26adcef7ec612806ad34535d4d6
Nishanthan V <nv@logitech.com>
2024-09-23 22:25:19 +0530

    Update integration name

--- obs_plugin/src/obs_plugin.hpp
+++ obs_plugin/src/obs_plugin.hpp
@@ -173,7 +173,7 @@ namespace logi
             std::condition_variable m_initialization_cv;
 
             // clang-format off
-            const std::string s_integration_name            = "OBS";
+            const std::string s_integration_name            = "APPLET_OBS_NAME";
             const std::string s_integration_author          = "Logitech G";
             const std::string s_integration_description     = "A Logitech G plugin for Open Broadcaster Software, exposing additional actions to G HUB.";
             const std::string s_integration_identifier      = "logi_obs_plugin";
